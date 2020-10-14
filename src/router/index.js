d13dec6f9bb44eac09a5bb7608b37716613fe992
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-10-14 10:01:21 +0100

    chore: initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,36 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Levels from "../views/Levels.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Levels1",
+    component: Levels,
+  },
+  {
+    path: "/levels",
+    name: "Levels",
+    component: Levels,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: () =>
+      import(/* webpackChunkName: "profile" */ "../views/Profile.vue"),
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
 });
 
 export default router;
